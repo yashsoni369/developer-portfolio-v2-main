16fa00dcacc43621423dca1ba2f620f0f5f0ecec
Yash Soni <yash.soni2737@gmail.com>
2024-09-06 23:33:43 +0530

    user specific changes

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -10,9 +10,6 @@
                 <NuxtLink :to="social.twitter.url + social.twitter.user" target="_blank" class="flex justify-center items-center">
                     <img src="/icons/social/twitter.svg"/>
                 </NuxtLink>
-                <NuxtLink :to="social.facebook.url + social.facebook.user" target="_blank" class="flex justify-center items-center">
-                    <img src="/icons/social/facebook.svg"/>
-                </NuxtLink>
                 <NuxtLink :to="social.github.url + social.github.user" target="_blank" class="flex md:hidden justify-center items-center">
                 <img src="/icons/social/github.svg"/>
             </NuxtLink>
